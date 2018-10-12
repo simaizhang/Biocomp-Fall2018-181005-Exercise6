8dbf1262ae1989acab40c8cb2dd0c0309429ea61
Brittni Bertolet <bbertole@nd.edu>
2018-10-12 12:59:49 -0400

    Update Exercise6.R

--- Exercise6.R
+++ Exercise6.R
@@ -4,7 +4,7 @@ file=read.csv(file="iris.csv",header=T,stringsAsFactors = F) # define the variab
 number <- 3 # define the variable 'number' with default value 3
 head(file,number) # display the first three of file
 
-### Brittni: We wanted you to recreate the functionality of head without using it. I took off 0.25 points, because you did still assign the variables. What we were looking for was something like:
+### Brittni: We wanted you to recreate the functionality of head without using it. What we were looking for was something like:
 ## file[1:number,]
 
 # part 2
